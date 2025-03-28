671992988d94579cb0bcb179c2707e211b538bbc
Lucian Hanga <lucianhanga@gmx.net>
2025-03-28 11:53:36 +0100

    Adjust user fetching interval to 10 seconds and add batch file to launch application

---
+++ status.app.bat
@@ -0,0 +1,7 @@
+@echo off
+@REM start chrome --incognito --app="http://localhost:3000"
+@REM start chrome --incognito --app="http://localhost:3000"
+@REM start chrome --incognito --app="https://adriansemafor123.z6.web.core.windows.net/"
+start chrome --profile-directory="NewProfile" --app="https://semafor2y00y.z6.web.core.windows.net/"
+@REM start chrome --profile-directory="NewProfile" --app="http://localhost:3000/"
+exit
